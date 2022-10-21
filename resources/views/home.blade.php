2e59312927b94f6ace2695d9af5733c884ef0c4c
Mandy <mandyms90@nauta.cu>
2022-10-21 19:24:39 -0400

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,31 @@
+ @extends('layouts.app')
+
+@section('content')
+    <section class="section">
+        <div class="section-header">
+            <h3 class="page__heading">Dashboard</h3>
+        </div>
+        <div class="section-body">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="card">
+                        <div class="card-body">
+                            
+                            <div class="card bt-c-blue order-card">
+                                <div class="card-blok">
+                                    <h5>Usuarios</h5>
+                                    <h2 class="text-right"><i class="fa fa-users f-left"></i><span></span></h2>
+                                    <p class="m-b-0 text-right"><a href="/usuarios" class="text-white">Ver más</a></p>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
+
