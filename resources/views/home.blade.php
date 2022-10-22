9552c1d3c50aad3fa4e31a813d941e80f66f90f5
Mandy <mandyms90@nauta.cu>
2022-10-22 17:14:29 -0400

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,17 +10,17 @@
                 <div class="col-lg-12">
                     <div class="card">
                         <div class="card-body">
-                            
-                            <div class="card bg-c-blue order-card">
-                                <div class="card-block">
-                                    <h5>Usuarios</h5>
-                                    <h2 class="text-right"><i class="fa fa-users f-left"></i><span></span></h2>
-                                    <p class="m-b-0 text-right"><a href="/usuarios" class="text-white">Ver más</a></p>
-                                    1111
+                            <div class="row">
+                                <div class="col-md-4-col-xl-4">
+                                    <div class="card bg-c-blue order-card">
+                                        <div class="card-block">
+                                            <h5>Usuarios</h5>
+                                            <h2 class="text-right"><i class="fa fa-users f-left"></i><span></span></h2>
+                                            <p class="m-b-0 text-right"><a href="/usuarios" class="text-white">Ver más</a></p>
+                                        </div>
+                                    </div>
                                 </div>
-
-                            </div>
-
+                            </div> 
                         </div>
                     </div>
                 </div>
