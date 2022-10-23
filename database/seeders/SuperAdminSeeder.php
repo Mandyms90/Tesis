d1cae767bff4610e6dcab4f88de106af825e48bb
Mandy <mandyms90@nauta.cu>
2022-10-22 20:39:05 -0400

    first commit

---
+++ database/seeders/SuperAdminSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class SuperAdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $usuario = User::create([
+            'name' => 'Super Admin',
+            'email' => 'amoreiras1990@yahoo.es',
+            'password' => bcrypt('shiroums1990')
+        ]);
+
+        // Estas lineas en caso de que no se haya creado el rol aun
+        // $rol = Role::create(['name'=>'Administrador']);
+        // $permisos = Permission::pluck('id','id')->all();
+        // $rol->syncPermissions($permisos);
+        // $usuario->assignRole([$rol->id]);
+        
+        // como ya el rol 'Administrador' esta creado:
+        $usuario->assignRole('Administrador');
+
+    }
+}
